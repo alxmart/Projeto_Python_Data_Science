fa8fb0297fda3468dc72bf3bcb18345bd510fb38
Luiz Alexandre F. Martinez <alxmartinez@gmail.com>
2025-04-25 07:25:31 -0300

    Add files via upload
    Exerc1 (Desafios Aula01).

---
+++ exerc1.py
@@ -0,0 +1,13 @@
+print('Escola de Dados da Alura')
+
+print('Nome: Luizão')
+print('Sobrenome: Mart')
+
+print('L')
+print('U')
+print('I')
+print('Z')
+
+print(31,'fevereiro', 2155)
+
+print('Ano atual:',2025)
